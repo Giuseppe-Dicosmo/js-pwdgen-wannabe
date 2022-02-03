f420e85118f94746df5b2742b78e668ba0224318
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-03 19:05:51 +0100

    js

--- myscript.js
+++ myscript.js
@@ -1,20 +1,25 @@
-const userName = prompt('Ciao, puoi dirmi il tuo nome?')
-console.log(userName)
+const userName = prompt('Ciao, puoi dirmi il tuo nome?');
+console.log(userName);
 
-const userSurname = prompt('È il tuo cognome?')
-console.log(userSurname)
+const userSurname = prompt('È il tuo cognome?');
+console.log(userSurname);
 
-const favoriteColor = prompt('Qual è il tuo colore preferito?')
-console.log(favoriteColor)
+const favoriteColor = prompt('Qual è il tuo colore preferito?');
+console.log(favoriteColor);
 
 let password = userName + userSurname + favoriteColor + '21';
-console.log(password)
+console.log(password);
 
-document.getElementById('saluto').innerHTML =
-'Ciao '
-+ userName
-+ ' ' + userSurname
-+ ' anche il mio colore preferito è il '
-+ favoriteColor
-+ ' ecco la tua password:'
-+ password;
+let salutoHTMLElement = document.getElementById('saluto');
+console.log(salutoHTMLElement);
+
+salutoHTMLElement.innerHTML += `Ciao ${userName} ${userSurname} anche il mio colore preferito è il ${favoriteColor} <br /> ecco la tua password:${password}`
+// document.getElementById('saluto').innerHTML =
+// 'Ciao '
+// + userName
+// + ' ' + userSurname
+// + ' anche il mio colore preferito è il '
+// + favoriteColor
+// + '<br />'
+// + ' ecco la tua password:'
+// + password;
