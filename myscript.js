241ebc92aa26768adda5bd80eb537525f95562c5
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-03 15:12:43 +0100

    utente

---
+++ myscript.js
@@ -0,0 +1,8 @@
+const userName = prompt('Ciao, puoi dirmi il tuo nome?')
+console.log(userName)
+
+const userSurname = prompt('È il tuo cognome?')
+console.log(userSurname)
+
+const favoriteColor = prompt('Qual è il tuo colore preferito?')
+console.log(favoriteColor)
